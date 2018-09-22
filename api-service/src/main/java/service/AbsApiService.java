dbe3dd72e8cb354945a905724704ca059358853e
Hieu Pham <hieupham@bitmark.com>
2018-09-22 11:44:12 +0700

    remove unnecessary callback reference

--- api-service/src/main/java/service/AbsApiService.java
+++ api-service/src/main/java/service/AbsApiService.java
@@ -4,11 +4,7 @@ import okhttp3.Headers;
 import okhttp3.Response;
 import service.params.Params;
 import service.params.query.QueryParams;
-import utils.callback.Callback;
 import utils.callback.Callback1;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author Hieu Pham
@@ -21,28 +17,16 @@ public abstract class AbsApiService {
 
     private HttpClient client;
 
-    private List<Callback> callbacks = new ArrayList<>();
-
     protected AbsApiService(String apiToken) {
         this.client = new HttpClientImpl(apiToken);
     }
 
-    protected void subscribe(Callback callback) {
-        if (!callbacks.contains(callback)) callbacks.add(callback);
-    }
-
-    public void unsubscribe(Callback callback) {
-        callbacks.remove(callback);
-    }
-
     protected void getAsync(String path, Callback1<Response> callback) {
         client.getAsync(path, callback);
-        subscribe(callback);
     }
 
     protected void getAsync(String path, QueryParams params, Callback1<Response> callback) {
         client.getAsync(path, params, callback);
-        subscribe(callback);
     }
 
     protected void postAsync(String path, Params params, Callback1<Response> callback) {
@@ -52,7 +36,6 @@ public abstract class AbsApiService {
     protected void postAsync(String path, Headers headers, Params params,
                              Callback1<Response> callback) {
         client.postAsync(path, headers, params, callback);
-        subscribe(callback);
     }
 
     protected void patchAsync(String path, Params params, Callback1<Response> callback) {
@@ -62,17 +45,14 @@ public abstract class AbsApiService {
     protected void patchAsync(String path, Headers headers, Params params,
                               Callback1<Response> callback) {
         client.patchAsync(path, headers, params, callback);
-        subscribe(callback);
     }
 
     protected void deleteAsync(String path, Callback1<Response> callback) {
         client.deleteAsync(path, callback);
-        subscribe(callback);
     }
 
     protected void deleteAsync(String path, Params params, Callback1<Response> callback) {
         client.deleteAsync(path, params, callback);
-        subscribe(callback);
     }
 
 }
