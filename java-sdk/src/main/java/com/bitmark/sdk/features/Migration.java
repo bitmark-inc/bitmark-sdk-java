557fb6bbfe82d04d26add7f4b8a3bd45f751e8b7
Hieu Pham <hieupham@bitmark.com>
2018-11-26 14:43:01 +0700

    Optimize migration code

--- java-sdk/src/main/java/com/bitmark/sdk/features/Migration.java
+++ java-sdk/src/main/java/com/bitmark/sdk/features/Migration.java
@@ -15,7 +15,9 @@ import com.bitmark.cryptography.error.ValidateException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 import static com.bitmark.apiservice.utils.Awaitility.await;
 import static com.bitmark.cryptography.utils.Validator.checkValid;
@@ -32,7 +34,8 @@ public class Migration {
 
     public static void migrate(String[] phraseWords,
                                Callback1<Pair<Account, List<String>>> callback) throws ValidateException {
-        checkValid(() -> phraseWords != null && phraseWords.length == TWENTY_FOUR.getMnemonicWordsLength(), "Only support migrate from 24 recovery words ");
+        checkValid(() -> phraseWords != null && phraseWords.length == TWENTY_FOUR.getMnemonicWordsLength(),
+                   "Only support migrate from 24 recovery words ");
         final Account oldAccount = Account.fromRecoveryPhrase(phraseWords);
         final String oldAccountNumber = oldAccount.getAccountNumber();
         final Account newAccount = new Account();
@@ -77,15 +80,16 @@ public class Migration {
         List<BitmarkRecord> firstBitmarks =
                 await((Callable1<GetBitmarksResponse>) internalCallback ->
                         Bitmark.list(new BitmarkQueryBuilder().ownedBy(accountNumber).pending(true).limit(1),
-                                internalCallback)).getBitmarks();
+                                     internalCallback)).getBitmarks();
         if (firstBitmarks == null || firstBitmarks.isEmpty()) return null;
         Long lastOffset = firstBitmarks.get(0).getOffset();
         while (lastOffset != null) {
 
             BitmarkQueryBuilder builder =
-                    new BitmarkQueryBuilder().ownedBy(accountNumber).at(lastOffset).to("earlier").loadAsset(true).pending(true).limit(limit);
+                    new BitmarkQueryBuilder().ownedBy(accountNumber).at(lastOffset).to("earlier").loadAsset(true)
+                                             .pending(true).limit(limit);
             GetBitmarksResponse response = await(internalCallback -> Bitmark.list(builder,
-                    internalCallback));
+                                                                                  internalCallback));
             result.add(response);
             final List<BitmarkRecord> bitmarks = response.getBitmarks();
             final int size = bitmarks == null ? 0 : bitmarks.size();
@@ -105,15 +109,15 @@ public class Migration {
         final KeyPair key = owner.getKey();
 
         // Build collection of IssuanceParams
-        List<IssuanceParams> params = new ArrayList<>();
-        assets.forEach(asset -> {
-            String assetId = asset.getId();
-            int quantity =
-                    (int) bitmarks.stream().filter(bitmark -> bitmark.getAssetId().equals(assetId)).count();
-            IssuanceParams param = new IssuanceParams(assetId, ownerAddress, quantity);
-            param.sign(key);
-            params.add(param);
-        });
+
+        List<IssuanceParams> params =
+                bitmarks.stream().collect(Collectors.groupingBy(BitmarkRecord::getAssetId, Collectors.counting()))
+                        .entrySet().stream().map(entry -> {
+                    IssuanceParams param =
+                            new IssuanceParams(entry.getKey(), ownerAddress, Math.toIntExact(entry.getValue()));
+                    param.sign(key);
+                    return param;
+                }).collect(Collectors.toList());
 
         // Execute from the above collection
         final List<String> bitmarkIds = new ArrayList<>();
