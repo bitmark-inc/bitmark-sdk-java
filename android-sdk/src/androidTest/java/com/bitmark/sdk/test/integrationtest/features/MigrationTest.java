c890074b718204b2fd3d7427de8ba74008961caa
Hieu Pham <hieupham@bitmark.com>
2019-01-09 11:38:05 +0700

    Clean redundant code

--- android-sdk/src/androidTest/java/com/bitmark/sdk/test/integrationtest/features/MigrationTest.java
+++ android-sdk/src/androidTest/java/com/bitmark/sdk/test/integrationtest/features/MigrationTest.java
@@ -11,8 +11,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static com.bitmark.apiservice.utils.Awaitility.await;
+import static com.bitmark.sdk.features.internal.Version.TWELVE;
 import static org.junit.Assert.*;
-import static com.bitmark.sdk.utils.Version.TWELVE;
 
 /**
  * @author Hieu Pham
@@ -29,22 +29,24 @@ public class MigrationTest extends BaseFeatureTest {
     public void testMigration_Valid24Words_CorrectValuesReturn() throws Throwable {
         List<String[]> twentyFourWords = new ArrayList<String[]>() {{
             add(("abuse tooth riot whale dance dawn armor patch tube sugar edit clean guilt " +
-                    "person" +
-                    " lake height tilt wall prosper episode produce spy artist account").split(" "
-            ));
+                 "person" +
+                 " lake height tilt wall prosper episode produce spy artist account").split(" "
+                                                                                           ));
             add(("accident syrup inquiry you clutch liquid fame upset joke glow best school " +
-                    "repeat" +
-                    " birth library combine access camera organ trial crazy jeans lizard science").split(" "));
+                 "repeat" +
+                 " birth library combine access camera organ trial crazy jeans lizard science")
+                        .split(" "));
         }};
 
         for (String[] words : twentyFourWords) {
             Pair<Account, List<String>> pair = await(callback -> Migration.migrate(words,
-                    callback), TIMEOUT);
+                                                                                   callback),
+                                                     TIMEOUT);
             Account account = pair.first();
             List<String> bitmarksId = pair.second();
             assertTrue(Account.isValidAccountNumber(account.getAccountNumber()));
             assertEquals(TWELVE.getMnemonicWordsLength(),
-                    account.getRecoveryPhrase().getMnemonicWords().length);
+                         account.getRecoveryPhrase().getMnemonicWords().length);
             assertNotNull(bitmarksId);
             assertFalse(bitmarksId.isEmpty());
         }
@@ -59,12 +61,13 @@ public class MigrationTest extends BaseFeatureTest {
             add(new String[]{});
             add(new String[24]);
             add(("during kingdom crew atom practice brisk weird document " +
-                    "eager artwork ride then").split(" "));
+                 "eager artwork ride then").split(" "));
         }};
         for (String[] words : twentyFourWords) {
             expectedException.expect(ValidateException.class);
             await((Callable1<Pair<Account, List<String>>>) callback -> Migration.migrate(words,
-                    callback), TIMEOUT);
+                                                                                         callback),
+                  TIMEOUT);
         }
 
     }
