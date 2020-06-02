a28462af3c1feb19917cd0888e5f484a16342875
Hieu Pham <hieupham@bitmark.com>
2020-06-02 14:02:09 +0700

    support compute fingerprint from multiple files/byte array

--- api-service/src/main/java/com/bitmark/apiservice/params/RegistrationParams.java
+++ api-service/src/main/java/com/bitmark/apiservice/params/RegistrationParams.java
@@ -10,12 +10,16 @@ import com.bitmark.apiservice.configuration.GlobalConfiguration;
 import com.bitmark.apiservice.utils.Address;
 import com.bitmark.apiservice.utils.BinaryPacking;
 import com.bitmark.apiservice.utils.FileUtils;
+import com.bitmark.cryptography.crypto.MerkleTree;
+import com.bitmark.cryptography.crypto.Sha3256;
 import com.bitmark.cryptography.crypto.Sha3512;
 import com.bitmark.cryptography.crypto.encoder.VarInt;
 import com.bitmark.cryptography.crypto.key.KeyPair;
+import com.bitmark.cryptography.utils.ArrayUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Base64;
 import java.util.Map;
 
 import static com.bitmark.cryptography.crypto.encoder.Hex.HEX;
@@ -60,6 +64,34 @@ public class RegistrationParams extends AbsSingleParams {
         return "01" + HEX.encode(hashedBytes);
     }
 
+    // should be bring out of main thread due to the heavy tasks
+    public static String computeFingerprint(byte[][] data) {
+        checkValid(
+                () -> data != null && data.length > 0,
+                "data must be non-null and not empty"
+        );
+        int length = data.length;
+        byte[][] hashes = new byte[length][];
+        for (int i = 0; i < length; i++) {
+            hashes[i] = Sha3512.hash(data[i]);
+        }
+
+        byte[][] merkleTree = MerkleTree.buildTree(
+                hashes,
+                (b1, b2) -> {
+                    byte[] b = ArrayUtils.concat(b1, b2);
+                    return length == 256 ? Sha3256.hash(b) : Sha3512.hash(b);
+                }
+        );
+
+        if (merkleTree.length == 0) {
+            throw new IllegalStateException("could not build merkle root");
+        }
+
+        byte[] root = merkleTree[merkleTree.length - 1];
+        return "02" + Base64.getEncoder().encodeToString(root);
+    }
+
     public static String getPackedMetadata(Map<String, String> metadata) {
         if (metadata == null) {
             return "";
@@ -98,12 +130,39 @@ public class RegistrationParams extends AbsSingleParams {
         return builder.toString();
     }
 
+    // should be bring out of main thread due to the heavy tasks
     public String setFingerprintFromFile(File file) throws IOException {
         checkValid(
                 () -> file != null && file.exists() && !file.isDirectory(),
                 "invalid file"
         );
         byte[] data = FileUtils.getBytes(file);
+        return fingerprint = computeFingerprint(data);
+    }
+
+    // should be bring out of main thread due to the heavy tasks
+    public String setFingerprintFromFiles(File[] files) throws IOException {
+        checkValid(
+                () -> files != null && files.length > 0,
+                "files must be non-null and not empty"
+        );
+        for (File f : files) {
+            checkValid(
+                    () -> f != null && f.exists() && !f.isDirectory(),
+                    "invalid file at " + f.getAbsolutePath()
+            );
+        }
+        int length = files.length;
+        byte[][] data = new byte[length][];
+        for (int i = 0; i < files.length; i++) {
+            data[i] = FileUtils.getBytes(files[i]);
+        }
+
+        return setFingerprintFromData(data);
+    }
+
+    // should be bring out of main thread due to the heavy tasks
+    public String setFingerprintFromData(byte[][] data) {
         return fingerprint = computeFingerprint(data);
     }
 
